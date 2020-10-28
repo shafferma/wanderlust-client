810442d49255c2efeb23db3834e6667c76f8d7a0
shafferma <shafferma12@gmail.com>
2020-10-28 18:26:06 -0400

    views and routes
    -set up routes
    -set up view folder
    -created nav bar with links
    -created router linking to pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./views/HomePage";
+import LoginPage from "./views/LoginPage";
+import TripViewPage from "./views/TripViewPage";
+import TripListPage from "./views/TripListPage";
+import RegistrationPage from "./views/RegistrationPage";
 
 function App() {
   return (
-    <div>
-      <h1>Test</h1>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/register">
+            <RegistrationPage />
+          </Route>
+          <Route path="/trips/:id">
+            <TripViewPage />
+          </Route>
+          <Route path="/trips">
+            <TripListPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
