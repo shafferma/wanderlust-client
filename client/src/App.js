79c8401db4142322e018d5ecc946b0b4983b54af
bendoane <ben.doane@gmail.com>
2020-10-28 20:28:02 -0400

    recent push

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,1 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Test</h1>
-      <h1>Robert test</h1>
-    </div>
-  );
-}
-
-export default App;
