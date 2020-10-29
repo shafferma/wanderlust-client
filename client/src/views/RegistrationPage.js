cba3e78ee79b4436129c98ef51039460ee8fe3ef
Ryan Krenzke <ryan.krenzke@gmail.com>
2020-10-29 13:10:06 -0400

    registration progress

--- client/src/views/RegistrationPage.js
+++ client/src/views/RegistrationPage.js
@@ -1,7 +1,66 @@
+import React, {useState} from 'react';
+import {Form, FormGroup, Label, Input, Button, ModalBody, Modal, ModalHeader} from 'reactstrap';
+import '../styles/RegistrationPage.css';
+
 const RegistrationPage = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirm, setPasswordConfirm] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(username && email && password){
+            if(password === passwordConfirm){
+
+            }else{
+                alert("Passwords do not match!");
+            }
+            fetch('herokudburl/user/register', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    username,
+                    email,
+                    password
+                }),
+            })
+            .then(response => response.json())
+            .catch(error => console.log(error));
+        }
+        //After successful registration, navigate to TRIP LIST PAGE
+    }
+
     return (
         // id and role are navigation from HomePage to Registration 
-        <div id="register" role='navigation'>Register</div>
+        <div id="register" role='navigation'>
+            <Modal id="registerModal">
+                <ModalHeader>Welcome to Wanderlust!</ModalHeader>
+                <ModalBody>
+                    <Form onSubmit={handleSubmit}>
+                        <FormGroup>
+                            <Label htmlFor="registerUsername">Username</Label>
+                            <Input onChange={event => setUsername(event.target.value)} value={username} id="registerUsername"></Input>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlFor="registerEmail">Email</Label>
+                            <Input onChange={event => setEmail(event.target.value)} value={email} id="registerEmail" type="email"></Input>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlFor="registerPassword"> Password</Label>
+                            <Input onChange={event => setPassword(event.target.value)} value={password} id="registerPassword" type="password"></Input>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlFor="passwordConfirm">Confirm Password</Label>
+                            <Input onChange={event => setPasswordConfirm(event.target.value)} value={passwordConfirm} id="passwordConfirm" type="password"></Input>
+                        </FormGroup>
+                        <Button>Submit</Button>
+                    </Form>
+                </ModalBody>
+            </Modal>
+        </div>
     )
 }
 
