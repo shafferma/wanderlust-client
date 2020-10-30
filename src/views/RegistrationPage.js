5b78b6e1204f578e1b3b1ac6a14691b05524b0bf
Ryan Krenzke <ryan.krenzke@gmail.com>
2020-10-30 09:35:15 -0400

    registration update

--- src/views/RegistrationPage.js
+++ src/views/RegistrationPage.js
@@ -1,53 +1,65 @@
 import React, { useState } from "react";
-import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { Form, FormGroup, Label, Input, Button, Modal, ModalBody, ModalHeader } from "reactstrap";
 import ApiProvider from "../utils/ApiProvider";
+import '../styles/RegistrationPage.css';
 
 const RegistrationPage = (props) => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [modalOpen, setModalOpen] = useState(true);
 
-  let handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    //reset error to false when resubmitting
-    setError(false);
-    ApiProvider.post("/register", {
-      username,
-      password,
-    })
-      .then((response) => {
-        props.updateToken(response.data.sessionToken);
-      })
-      .catch((error) => {
-        setError(error.response.data);
-      });
-  };
+    if(username && email && password){
+      if(password === passwordConfirm){
+        fetch("https://wanderlust-travel-hhsk.herokuapp.com/user/register", {
+          method: 'POST',
+          headers: {
+          "Content-Type": "application/json"
+          },
+          body: JSON.stringify({user: {username, email, password}}),
+          })
+          .then(response => response.json())
+          .then((data) => {
+              props.updateToken(data.sessionToken)
+          })
+          .catch(error => console.log(error));
+          }else{
+            alert("Passwords do not match!");
+          }
+        }
+        //After successful registration, navigate to TRIP LIST PAGE
+    };
 
   return (
-    <div>
-      <h1>Register</h1>
-      <Form onSubmit={handleSubmit}>
-        <FormGroup>
-          <Label htmlFor="username">Username</Label>
-          <Input
-            onChange={(e) => setUsername(e.target.value)}
-            name="username"
-            value={username}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label htmlFor="password">Password</Label>
-          <Input
-            onChange={(e) => setPassword(e.target.value)}
-            name="password"
-            type="password"
-            value={password}
-          />
-        </FormGroup>
-        <Button type="submit">Signup</Button>
-        {error ? <p style={{ color: "red" }}>{error}</p> : null}
-      </Form>
-    </div>
+    <div id="register" role='navigation'>
+            <Modal isOpen={modalOpen} id="registerModal">
+                <ModalHeader>Welcome to Wanderlust!</ModalHeader>
+                <ModalBody>
+                    <Form onSubmit={handleSubmit}>
+                        <FormGroup>
+                            <Label htmlFor="registerUsername">Username</Label>
+                            <Input onChange={event => setUsername(event.target.value)} value={username} id="registerUsername"></Input>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlFor="registerEmail">Email</Label>
+                            <Input onChange={event => setEmail(event.target.value)} value={email} id="registerEmail" type="email"></Input>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlFor="registerPassword"> Password</Label>
+                            <Input onChange={event => setPassword(event.target.value)} value={password} id="registerPassword" type="password"></Input>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlFor="passwordConfirm">Confirm Password</Label>
+                            <Input onChange={event => setPasswordConfirm(event.target.value)} value={passwordConfirm} id="passwordConfirm" type="password"></Input>
+                        </FormGroup>
+                        <Button>Create Account</Button>
+                    </Form>
+                </ModalBody>
+            </Modal>
+        </div>
   );
 };
 
