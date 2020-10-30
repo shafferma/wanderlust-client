db325084cdcd09dfa1b57ccdf786daf8a3d73e45
Ryan Krenzke <ryan.krenzke@gmail.com>
2020-10-30 16:39:09 -0400

    register and login upgrade

--- src/App.js
+++ src/App.js
@@ -28,6 +28,11 @@ function App() {
     localStorage.clear();
     setSessionToken("");
   };
+
+  // const protectedViews = () => {
+  //   return (sessionToken === window.localStorage.getItem("token") ? <TripListPage token={sessionToken} /> : 
+  //   <RegistrationPage updateToken={updateToken}/>)
+  // }
   
   return (
     <Router>
