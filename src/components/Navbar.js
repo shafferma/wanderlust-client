9216f6178d24ea154becfef4baf2fd39ef0990e0
shafferma <shafferma12@gmail.com>
2020-10-30 10:51:56 -0400

    deleted duplicate nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,3 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-
-import { Button } from "reactstrap";
-
-const Navbar = () => {
-  return (
-    
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/trips">Trips</Link>
-        </li>
-        <li>
-          <Link to="/search">Search</Link>
-        </li>
-        <li>
-          <Button>Logout</Button>
-        </li>
-      </ul>
-    </nav>
-  );
-};
-
-export default Navbar;
-
 import {
   Nav,
   NavLink,
